8feaeddea685610dc1b60ad590a3f16165830665
Aryan <alikhaniaryan@live.com>
2018-04-01 03:46:57 +0430

    documenting src/startup.js done.

--- src/startup.js
+++ src/startup.js
@@ -1,9 +1,21 @@
+/*
+    Filename: src/startus.js
+    Author: Aryan Alikhani
+    Last Edit: April 1 2018, 3:43 AM
 
+    Description: startup call after loading Store.
+
+    Issues:
+        [ ] saving languages to global variable may not be an intelligence way to handle localization but it works.
+
+ */
 
 
 import {Helpers} from '../core';
 import {APP_LANGUAGE, APP_LOCALES} from "../core/i18";
 
+// Load locales from local directory.
+// you must keep this pattern for other languages too.
 const Locales = {
     fa: require('./locales/fa').default,
     en: require('./locales/en').default,
@@ -11,6 +23,7 @@ const Locales = {
 
 export default function (state, store) {
 
+    // Loading application stored language from memory.
     const lang = Helpers.leaf(state, 'app.lang');
     global[APP_LANGUAGE] = lang;
     global[APP_LOCALES] = Locales[lang];
